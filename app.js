91cb31b95f68cd66df45b7af19e096d24936d395
Rituraj <riturajguwahati26@gmail.com>
2020-07-26 19:18:35 +0530

    env variable setup

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var passport = require("passport");
-var passportLocal = require("passport-local");
-var expressSession = require("express-session");
-var User = require("./models/user");
-var methodOverride = require("method-override");
-var flash = require("connect-flash");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const passportLocal = require("passport-local");
+const expressSession = require("express-session");
+const User = require("./models/user");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db")
 
-var indexRoutes = require('./routes/index');
-var ecotourismRoutes = require('./routes/ecotourism');
-var commentRoutes = require('./routes/comments');
+// Env Var Configuration
+dotenv.config({ path: './config/config.env' });
 
-// CONFIG
+// Database Connection
+connectDB();
 
-mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://localhost/ecotourism");
+const indexRoutes = require('./routes/index');
+const ecotourismRoutes = require('./routes/ecotourism');
+const commentRoutes = require('./routes/comments');
 
 app.use(flash());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 app.use(expressSession({
-    secret: "Ecotourism are really an interesting thing",
-    resave: false,
-    saveUninitialized: false
+	secret: "Ecotourism are really an interesting thing",
+	resave: false,
+	saveUninitialized: false
 }));
 
 app.use(passport.initialize());
@@ -36,18 +38,19 @@ passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    next();
+app.use(function (req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
 });
 
 app.use("/", indexRoutes);
 app.use("/ecotourism", ecotourismRoutes);
 app.use("/ecotourism/:id/comments", commentRoutes);
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, process.env.IP);
-
-// app.listen(3000);
+app.listen(PORT, () => {
+	console.log(`Server Started at ${PORT}`);
+})
